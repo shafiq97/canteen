0828d7541a82097c2008e519ad77a2691bed6481
Muhammad Shafiq Ahmad Razman <shafiq@Muhammads-MacBook-Air.local>
2023-12-09 10:57:31 +0800

    latset

--- cust_regist.php
+++ cust_regist.php
@@ -2,7 +2,9 @@
 <html lang="en">
 
 <head>
-    <?php session_start(); include("conn_db.php"); include('head.php');?>
+    <?php session_start();
+    include("conn_db.php");
+    include('head.php'); ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,7 +17,7 @@
     <header class="navbar navbar-light fixed-top bg-light shadow-sm mb-auto">
         <div class="container-fluid mx-4">
             <a href="index.php">
-            <img src="https://www.gmi.edu.my/wp-content/uploads/2019/06/logo-gmi-header.png" width="125" class="me-2" alt="EATERIO Logo">
+                <img src="https://www.gmi.edu.my/wp-content/uploads/2019/06/logo-gmi-header.png" width="125" class="me-2" alt="EATERIO Logo">
             </a>
         </div>
     </header>
@@ -24,29 +26,29 @@
         <a class="nav nav-item text-decoration-none text-muted" href="#" onclick="history.back();">
             <i class="bi bi-arrow-left-square me-2"></i>Go back
         </a>
+        <div id="nfcPrompt" style="display: none;">
+            <p class="alert alert-info">Please tap your NFC card</p>
+        </div>
+        <div id="nfcData"></div>
         <form method="POST" action="add_cust.php" class="form-floating">
             <h2 class="mt-4 mb-3 fw-normal text-bold"><i class="bi bi-person-plus me-2"></i>Sign Up</h2>
             <div class="form-floating mb-2">
-                <input type="text" class="form-control" id="username" placeholder="Username" name="username"
-                    minlength="5" maxlength="45" required>
+                <input type="text" class="form-control" id="username" placeholder="Username" name="username" minlength="5" maxlength="45" required>
                 <label for="username">Username</label>
             </div>
             <div class="form-floating mb-2">
-                <input type="password" class="form-control" id="pwd" placeholder="Password" name="pwd" minlength="8"
-                    maxlength="45" required>
+                <input type="password" class="form-control" id="pwd" placeholder="Password" name="pwd" minlength="8" maxlength="45" required>
                 <label for="pwd">Password</label>
             </div>
             <div class="form-floating mb-2">
-                <input type="password" class="form-control" id="cfpwd" placeholder="Confirm Password" minlength="8"
-                    maxlength="45" name="cfpwd" required>
+                <input type="password" class="form-control" id="cfpwd" placeholder="Confirm Password" minlength="8" maxlength="45" name="cfpwd" required>
                 <label for="cfpwd">Confirm Password</label>
                 <div id="passwordHelpBlock" class="form-text smaller-font">
                     Your password must be at least 8 characters long.
                 </div>
             </div>
             <div class="form-floating mb-2">
-                <input type="text" class="form-control" id="firstname" placeholder="First Name" name="firstname"
-                    required>
+                <input type="text" class="form-control" id="firstname" placeholder="First Name" name="firstname" required>
                 <label for="firstname">First Name</label>
             </div>
             <div class="form-floating mb-2">
@@ -89,14 +91,41 @@
         </form>
     </div>
     <div class="container mt-4"></div>
-    <footer
-        class="footer d-flex flex-wrap justify-content-between align-items-center px-5 py-3 mt-auto bg-secondary text-light">
+    <footer class="footer d-flex flex-wrap justify-content-between align-items-center px-5 py-3 mt-auto bg-secondary text-light">
         <span class="smaller-font">&copy; 2023 German Malaysian Institue<br /><span class="xsmall-font">Rezza</span></span>
         <ul class="nav justify-content-end list-unstyled d-flex">
-            <li class="ms-3"><a class="text-light" target="_blank" href=""><i
-                        class="bi bi-github"></i></a></li>
+            <li class="ms-3"><a class="text-light" target="_blank" href=""><i class="bi bi-github"></i></a></li>
         </ul>
     </footer>
+    <script>
+        document.getElementById('type').addEventListener('change', function() {
+            var typeSelect = document.getElementById('type');
+            var nfcPrompt = document.getElementById('nfcPrompt');
+            if (typeSelect.value === 'STD') {
+                nfcPrompt.style.display = 'block';
+            } else {
+                nfcPrompt.style.display = 'none';
+            }
+        });
+
+        function checkForNfcData() {
+            fetch('http://localhost:5001/get_uid')
+                .then(response => response.json())
+                .then(data => {
+                    if (data.uid) {
+                        // Convert UID to string and remove commas
+                        var uidString = String(data.uid).replace(/,/g, '');
+                        document.getElementById('username').value = uidString;
+                    }
+                })
+                .catch(error => {
+                    console.error('Error fetching NFC data:', error);
+                });
+        }
+
+        setInterval(checkForNfcData, 1000); // Check every 2 seconds
+    </script>
+
 </body>
 
 </html>
